282eb72236f3b72c2fe63b4eec87f1d18f7396fc
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-07-30 20:00:43 +0900

    search追加

--- routes/api.php
+++ routes/api.php
@@ -15,31 +15,25 @@ use App\Http\Controllers\Api\Member\MyVillage\MyVillageApiController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 use App\Http\Controllers\Api\Member\Village\VillageApiController;
 use App\Http\Controllers\Api\Member\Village\VillageValidationApiController;
-
-Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::apiResource('/member/village', VillageApiController::class);
-
-    //省略
-});
-
+// Route::group(['middleware' => 'auth:sanctum'], function () {
+//     Route::apiResource('/member/village', VillageApiController::class);
+//     //省略
+// });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 // 会員登録
 Route::post('/auth/register', [RegisterApiController::class, 'register']);
 // ログイン
 Route::post('/auth/login', [LoginApiController::class, 'Login']);
-
 // ログイン後
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/village', VillageApiController::class);
+    Route::post('/village/join', [VillageApiController::class, 'join']);
     // ビレッジ登録時バリデーション
     Route::post('/village/register/validation/topic', [VillageValidationApiController::class, 'topic']);
     Route::post('/village/register/validation/setting', [VillageValidationApiController::class, 'setting']);
-
     Route::apiResource('/my/village', MyVillageApiController::class);
 });
