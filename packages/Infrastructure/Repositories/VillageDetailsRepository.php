e6c5909063187d363abc404a5b6ce9fb816bd4c2
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-08-20 22:30:35 +0000

    朝push

--- packages/Infrastructure/Repositories/VillageDetailsRepository.php
+++ packages/Infrastructure/Repositories/VillageDetailsRepository.php
@@ -2,7 +2,7 @@
 
 namespace Packgage\Infrastructure\Repositories;
 
-
+use App\Models\VillageMember as ModelsVillageMember;
 use Packages\Domain\Interfaces\Repositories\VillageDetailsRepositoryInterface;
 use Packages\Domain\Models\Village\Village;
 
@@ -10,7 +10,13 @@ class VillageDetailsRepository implements VillageDetailsRepositoryInterface
 {
     public function get(Village $village): Village
     {
+        $village_id = $village->village_id;
+        $village_member_info = $this->village_member_info->get($village_id);
+        $core_members = $village_member_info->coreMembers();
+        $rise_members = $village_member_info->riseMember();
 
+        foreach ($core_members as $core_member) {
+        }
         return $village;
     }
 
