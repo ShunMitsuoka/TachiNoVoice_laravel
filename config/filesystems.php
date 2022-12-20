2e0b8e2af0995055d912326da8ca6255c002ed2a
s.mitsuoka <s.mitsuoka@and-ex.co.jp>
2022-12-20 06:12:13 +0000

    修正

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,14 +33,6 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'permissions' => [ // permissions 設定値を追記
-                'dir' => [
-                    'public'  => 0775, // public なディレクトリは775で作成 
-                ],
-                'file' => [
-                    'public' => 0664, // public なファイルは664で作成
-                ],
-            ],
             'throw' => false,
         ],
 
@@ -50,6 +42,14 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
+            'permissions' => [ // permissions 設定値を追記
+                'dir' => [
+                    'village'  => 0775, // public なディレクトリは775で作成 
+                ],
+                'file' => [
+                    'village' => 0664, // public なファイルは664で作成
+                ],
+            ],
         ],
 
         's3' => [
