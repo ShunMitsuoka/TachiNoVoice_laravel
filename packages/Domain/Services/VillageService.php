54bb260d9d51df809898b2c0c95b9ac74843e32e
ShunMitsuoka <physics.sm0414@gmail.com>
2023-02-04 07:46:53 +0000

    終了処理修正

--- packages/Domain/Services/VillageService.php
+++ packages/Domain/Services/VillageService.php
@@ -136,8 +136,8 @@ class VillageService{
             if(!$village->phase()->isLastPhase()){
                 $village->nextPhase();
                 $updated_village = $this->village_repository->update($village);
+                $this->send_next_phase_email_service->sendNextPhaseEmail($updated_village);
             }
-            $this->send_next_phase_email_service->sendNextPhaseEmail($updated_village);
             DB::commit();
             return $updated_village;
         } catch (\Throwable $th) {
